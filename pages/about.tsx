0e1c6d3250d2a1a903f094c1e6b6fe5de5b04f5f
CodeDroid99 <johnmwega2002@gmail.com>
2023-08-07 16:55:35 +0300

    style all the pages

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,21 +1,27 @@
+import HowToUseSection from "@/components/HowToUse";
+
 const AboutPage: React.FC = () => {
     return (
-      <div className="flex min-h-screen justify-center items-center">
-        <div className="p-8 border border-gray-300 rounded-md shadow-lg max-w-md">
-          <h2 className="text-2xl font-semibold mb-4">About the Tool</h2>
-          <p className="text-gray-700">
-            The graphical authentication system is a web-based tool designed to provide a secure and user-friendly way for users to authenticate and access their accounts. Instead of traditional text-based passwords, this tool uses graphical elements, such as images or patterns, to authenticate users, making it more difficult for unauthorized individuals to gain access to user accounts.
-          </p>
-          <p className="text-gray-700 mt-4">
-            With the graphical authentication system, users can create and register their unique graphical passwords, which are then used during login attempts. The system utilizes advanced image recognition algorithms and pattern matching techniques to verify the authenticity of the entered graphical password.
-          </p>
-          <p className="text-gray-700 mt-4">
-            Our primary goal is to enhance the security of user accounts while providing an intuitive and user-friendly authentication experience. We believe that graphical authentication can be an effective alternative to traditional text-based passwords and contribute to a safer online environment for users.
-          </p>
+        <div className="flex  flex-col justify-center items-center w-full ">
+            <div className="bg-blue-200 p-8 px-20 ">
+                <div className="relative isolate px-6 pt-10 lg:px-8">
+                    <h2 className="text-2xl text-center p-5 font-semibold mb-4 text-black">About the Tool</h2>
+                    <p className="text-neutral-500 text-center">
+                        The graphical authentication system is a web-based tool designed to provide a secure and user-friendly way for users to authenticate and access their accounts. Instead of traditional text-based passwords, this tool uses graphical elements, such as images or patterns, to authenticate users, making it more difficult for unauthorized individuals to gain access to user accounts.
+                    </p>
+                    <p className="text-neutral-500 text-center w-full mt-1">
+                        With the graphical authentication system, users can create and register their unique graphical passwords, which are then used during login attempts. The system utilizes advanced image recognition algorithms and pattern matching techniques to verify the authenticity of the entered graphical password.
+                    </p>
+                    <p className="text-neutral-500 mt-1">
+                        Our primary goal is to enhance the security of user accounts while providing an intuitive and user-friendly authentication experience. We believe that graphical authentication can be an effective alternative to traditional text-based passwords and contribute to a safer online environment for users.
+                    </p>
+                </div>
+            </div>
+            <div>
+                <HowToUseSection />
+            </div>
         </div>
-      </div>
     );
-  };
-  
-  export default AboutPage;
-  
+};
+
+export default AboutPage;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import HowToUseSection from '@/components/HowToUse';
 import Image from 'next/image';
 
 export default function Home() {
@@ -72,21 +73,25 @@ export default function Home() {
           <div className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80" aria-hidden="true">
             <div className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]" style={{ clipPath: "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)" }}></div>
           </div>
-          <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
+          <div className="mx-auto max-w-2xl py-16 sm:py-48 lg:py-36">
             
             <div className="text-center">
-              <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-4xl">Data to enrich your online business</h1>
+              <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-4xl">Unlock Your World with Pictures</h1>
+              <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-4xl">Your Password, Your Canvas!</h1>
               <p className="mt-6 text-lg leading-8 text-gray-600">
               Graphical Password Auth Tool is an innovative and user-friendly authentication system designed to enhance security while providing a memorable and engaging user experience. Unlike traditional text-based passwords, this tool employs a visually appealing approach that allows dyslexic users to create and authenticate passwords using images, patterns, or shapes.</p>
               <div className="mt-10 flex items-center justify-center gap-x-6">
-                <a href="#" className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Get started</a>
-                <a href="#" className="text-sm font-semibold leading-6 text-gray-900">Learn more <span aria-hidden="true">→</span></a>
+                <a href="sign-up" className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Get started</a>
+                <a href="about" className="text-sm font-semibold leading-6 text-gray-900">Learn more <span aria-hidden="true">→</span></a>
               </div>
             </div>
           </div>
           <div className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]" aria-hidden="true">
             <div className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]" style={{ clipPath: "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)" }}></div>
           </div>
+        </div>
+        <div>
+          <HowToUseSection />
         </div>
       </div>
     </div>
