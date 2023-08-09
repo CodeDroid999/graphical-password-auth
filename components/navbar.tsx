aff67617cd9493b03b3185a33312d656f7886286
CodeDroid99 <johnmwega2002@gmail.com>
2023-08-09 16:04:59 +0300

    remove flowbite logo

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -6,15 +6,7 @@ export default function Navbar() {
         <nav className="bg-white dark:bg-blue-900 fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600">
             <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
                 <a href="https://flowbite.com/" className="flex items-center">
-                    <Image
-                        src="https://flowbite.com/docs/images/logo.svg"
-                        width="168"
-                        height="168"
-                        className="h-8 mr-3"
-                        alt="Flowbite Logo"
-                    />
-
-                    <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">GPA</span>
+                                      <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">GPA</span>
                 </a>
                 <div className="flex md:order-2">
                     <button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"><a href="log-in">Log in</a></button>
