7c2e48a349251363e8c96a5c951285604b88c312
CodeDroid99 <johnmwega2002@gmail.com>
2023-08-11 10:55:21 +0300

    ;m,zd;,mb;a,b;

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -5,7 +5,7 @@ export default function Navbar() {
     return (
         <nav className="bg-white dark:bg-blue-900 fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600">
             <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-                <a href="https://flowbite.com/" className="flex items-center">
+                <a href="" className="flex items-center">
                                       <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">GPA</span>
                 </a>
                 <div className="flex md:order-2">
